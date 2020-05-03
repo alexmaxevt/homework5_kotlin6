6a497d44f3affb62141778b16cb26b075d9a3857
alexmaxevt <alexeymaxevt@yandex.ru>
2020-05-03 09:38:54 +0300

    добавлены получение данных через json и ProgressBar

--- app/src/main/java/ru/evtukhov/android/homework6/MainActivity.kt
+++ app/src/main/java/ru/evtukhov/android/homework6/MainActivity.kt
@@ -1,28 +1,44 @@
 package ru.evtukhov.android.homework6
 
 import android.os.Bundle
+import android.view.View.GONE
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import io.ktor.client.request.get
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
 import ru.evtukhov.android.homework6.adapter.PostAdapter
+import ru.evtukhov.android.homework6.client.Api
 import ru.evtukhov.android.homework6.posts.Post
-import ru.evtukhov.android.homework6.posts.PostType
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
 
+    private val job = SupervisorJob()
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
+
+    @io.ktor.util.KtorExperimentalAPI
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val list = listOf(
-            Post(1, "Netology", "First post in our network!", 1585637381, false, 0, false, 50, false, 10, postType = PostType.POST),
-            Post(2, "Netology", "First post in our network!", 1585637381, false, 0, false, 50, false, 10, "address", 55.75222, 37.61556, postType = PostType.EVENT),
-            Post(3, "Netology", "First post in our network!", 1585637381, false, 0, false, 50, false, 10, null, null, null, postType = PostType.VIDEO, videoLink = "https://vimeo.com/27299211"),
-            Post(4, "Netology", "First post in our network!", 1585637381, false, 0, false, 50, false, 10, null, null, null, postType = PostType.ADVERTISING, videoLink = null, intentLink = "https://netology.ru/?utm_source=yandex&utm_medium=cpc&utm_campaign=brand_all_ou_ya_retarget&utm_content=8944030781&utm_term=нетология&yclid=1932640884155777924&stop=1")
-        )
-        with(itemList) {
-            layoutManager = LinearLayoutManager(this@MainActivity)
-            adapter = PostAdapter(list)
+        launch {
+            val list = withContext(Dispatchers.IO) {
+                Api.client.get<List<Post>>(Api.urlJson)
+            }
+            with(itemList) {
+                layoutManager = LinearLayoutManager(this@MainActivity)
+                adapter = PostAdapter(list)
+            }
+            progress_circular.visibility = GONE
+            Toast.makeText(this@MainActivity, "Length: ${list.size}", Toast.LENGTH_LONG).show()
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cancel()
     }
 }
